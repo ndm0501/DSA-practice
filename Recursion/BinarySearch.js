1edd921fca2a2c91c1546d841c993e2b6ca8486c
Nadeem <nadeem@ip-192-168-0-102.ap-south-1.compute.internal>
2021-11-04 14:49:17 +0530

    binary search

---
+++ Recursion/BinarySearch.js
@@ -0,0 +1,26 @@
+/*
+Works on list of sorted arrays
+-1 0 1 2 3 4 7 9 10 20
+
+*/
+
+function binarySearch(arr, left, right, value){
+    let mid = Math.floor((left+right)/2);
+    
+    if(right<left){
+        return -1;
+    }
+    if(arr[mid] == value){
+        return true;
+    }else if(arr[mid]>value){
+        right = mid-1;
+        return binarySearch(arr, left, right, value)
+    }else{
+        left = mid+1;
+        return binarySearch(arr, left, right, value)
+    }
+
+}
+const arr = [-1, 0, 1, 2, 3, 4, 7, 9, 10, 20];
+
+console.log(binarySearch(arr, 0, arr.length, 4))
