43022ba0aa1d43487d3fa05e0dff6a4ef9ace9db
ndm0501 <nadeem.endeavour@gmail.com>
2022-10-08 12:50:59 +0530

    adding short circuit for same frequency

--- FrequencyCounter/sameFrequency.js
+++ FrequencyCounter/sameFrequency.js
@@ -19,12 +19,14 @@ function sameFrequency(num1, num2) {
     // good luck. Add any arguments you deem necessary.
     const str1 = num1.toString();
     const str2 = num2.toString();
+    const str1len = str1.length;
+    const str2len = str2.length;
     const charMap = {};
 
-    for (let i = 0; i < str1.length; i++) {
+    if (str1len !== str2len) return false;
+
+    for (let i = 0; i < str1len; i++) {
         charMap[str1[i]] = (charMap[str1[i]] || 0) + 1;
-    }
-    for (let i = 0; i < str2.length; i++) {
         charMap[str2[i]] = (charMap[str2[i]] || 0) - 1;
     }
 
@@ -37,4 +39,5 @@ function sameFrequency(num1, num2) {
     return true;
 }
 
-console.log(sameFrequency(22,222) )
+console.log(sameFrequency(22, 222));
+console.log(sameFrequency(3589578, 5879385));
